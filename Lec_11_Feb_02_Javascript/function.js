d3aedab25dd3b8fbb87df63009e14a95f70df72c
MohdShavezz <mohdpctebtech19cse@gmail.com>
2026-02-02 12:56:10 +0530

    function declaration expression parameter arguments anonyms fn arrow fn IIFE closure fn currying higher order function GEC( global exec context) Call Stack Execution

---
+++ Lec_11_Feb_02_Javascript/function.js
@@ -0,0 +1,105 @@
+// function fn() { //function declaration
+//   console.log('this is fn')
+// }
+// fn()
+// fn()
+// fn()
+// fn()
+// fn()
+
+// function sum(a,b){ // params
+//   // console.log(a+b)
+//   return a+b
+// }
+
+// let r=sum(1,2) //arguments
+// console.log(r)
+
+// function declaration vs function expression
+
+// let a=function(){ // function expression
+//   console.log('anonyms function')
+// }
+// a()
+
+// arrow function
+// let b=()=>{
+//   console.log('arrow function')
+// }
+// b()
+
+// normal()
+// a()
+
+// function normal(){
+//   console.log('normal function')
+// }
+// let a=function(){ // function expression
+//   console.log('anonyms function')
+// }
+
+
+//callbackfn
+// function fn1(cb){
+//   console.log('htis is fn1')
+//   cb()
+// }
+// // function fn2(){
+// //   console.log('fn2')
+// // }
+// fn1(()=>{
+//   console.log('fn2')
+// })
+
+
+// higher order function
+// function fn1(){
+//   return function(){}
+// }
+// fn1(fn2)
+
+//closure
+// function fn1(){
+//   let a=5 //lexical scope
+  
+//   return function(){
+//     console.log(a)
+//     a++
+//   }  
+// }
+// let a=fn1()
+// a()
+// a()
+// a()
+
+
+//fn currying
+// function sum(a){
+//   return function(b){
+//     return function(c){
+//       return a+b+c
+//     }
+//   }
+// }
+// console.log(sum(1)(2)(3))
+
+//execution of js debugging
+let r=10
+function fn1(){
+  let a=1
+  console.log('this is fn',a)
+  fn2()
+}
+function fn2(){
+  let a=2
+  console.log('this is fn',a)
+  fn3()
+}
+function fn3(){
+  let a=3
+  console.log('this is fn',a)
+}
+
+fn1()
+console.log(r)
+
